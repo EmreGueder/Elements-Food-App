14ad68bc802a819d9515c729454ab83347ac4201
EmreR7 <emre_guder@hotmail.de>
2020-11-21 17:20:50 +0100

    Initial Commit

--- app/src/main/java/com/example/elementsfoodapp/MainActivity.java
+++ app/src/main/java/com/example/elementsfoodapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.elementsfoodapp;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
---
+++ app/src/main/java/com/example/elementsfoodapp/SearchableActivity.java
@@ -0,0 +1,13 @@
+package com.example.elementsfoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
