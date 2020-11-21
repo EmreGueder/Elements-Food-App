94de5ded9c97789988aa80e5ab349c08d3af6eff
EmreR7 <emre_guder@hotmail.de>
2020-11-21 22:50:10 +0100

    Initial Commit

--- app/src/main/java/com/example/elementsfoodapp/MainActivity.java
+++ app/src/main/java/com/example/elementsfoodapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.elementsfoodapp;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.LabelVisibilityMode;
@@ -25,9 +31,23 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_favorites, R.id.navigation_global_search,
                 R.id.navigation_categories)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavController navController = Navigation.findNavController(
+                this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(
+                this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        //Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        return true;
+    }
 }
--- app/src/main/java/com/example/elementsfoodapp/SearchableActivity.java
+++ app/src/main/java/com/example/elementsfoodapp/SearchableActivity.java
@@ -2,6 +2,8 @@ package com.example.elementsfoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SearchableActivity extends AppCompatActivity {
@@ -9,5 +11,21 @@ public class SearchableActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.search);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        //Get the intent, verify the action and get the query
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //doMySearch(query);
+        }
     }
 }
