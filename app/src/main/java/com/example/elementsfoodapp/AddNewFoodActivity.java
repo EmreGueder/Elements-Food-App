9d4602379b6fe5a055d783a89a8ab3167d28f43b
EmreR7 <emre_guder@hotmail.de>
2020-12-15 00:45:45 +0100

    Initial Commit

--- app/src/main/java/com/example/elementsfoodapp/AddNewFoodActivity.java
+++ app/src/main/java/com/example/elementsfoodapp/AddNewFoodActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddNewFoodActivity extends AppCompatActivity {
 
@@ -12,6 +14,12 @@ public class AddNewFoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_food);
+
+        Spinner spinner = (Spinner) findViewById(R.id.selectFoodElement);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.food_elements_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/example/elementsfoodapp/ui/categories/CategoriesFragment.java
+++ app/src/main/java/com/example/elementsfoodapp/ui/categories/CategoriesFragment.java
@@ -1,8 +1,5 @@
 package com.example.elementsfoodapp.ui.categories;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,25 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.elementsfoodapp.R;
 
 public class CategoriesFragment extends Fragment {
 
-    private CategoriesViewModel categoriesViewModel;
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        categoriesViewModel =
-                new ViewModelProvider(this).get(CategoriesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_categories, container, false);
-        final TextView textView = root.findViewById(R.id.text_categories);
-        categoriesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) { textView.setText(s); }
-        });
         return root;
     }
 }
