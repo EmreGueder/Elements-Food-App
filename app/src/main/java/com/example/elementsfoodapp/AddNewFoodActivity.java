a7e56e02980cb356e70ea4acd2f16fca60964abd
EmreR7 <emre_guder@hotmail.de>
2020-12-19 21:03:43 +0100

    Initial Commit

--- app/src/main/java/com/example/elementsfoodapp/AddNewFoodActivity.java
+++ app/src/main/java/com/example/elementsfoodapp/AddNewFoodActivity.java
@@ -1,25 +1,30 @@
 package com.example.elementsfoodapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddNewFoodActivity extends AppCompatActivity {
+
+    String[] elements = new String[]{"Wood", "Fire", "Earth", "Metal", "Water"};
+    boolean[] checkedElements = new boolean[]{false, false, false, false, false};
+
+    TextInputEditText elementsEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_food);
 
-        Spinner spinner = (Spinner) findViewById(R.id.selectFoodElement);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.food_elements_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        elementsEditText = (TextInputEditText) findViewById(R.id.selectFoodElement);
     }
 
     @Override
@@ -27,5 +32,48 @@ public class AddNewFoodActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.add_new_food_options_menu, menu);
         return true;
+    }
+
+    public void openDialog(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //Setting AlertDialog Characteristics
+        builder.setTitle("Select items");
+
+        //Building the list to be shown in AlertDialog
+        builder.setMultiChoiceItems(
+                elements, checkedElements, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                //Update the current item's checked status
+                checkedElements[which] = isChecked;
+            }
+        });
+
+        //Set positive button
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                for (int i = 0; i<checkedElements.length; i++) {
+                    boolean checked = checkedElements[i];
+                    if (checked) {
+                        elementsEditText.setText(getString(R.string.display_items, elements[i]));
+                    }
+                }
+            }
+        });
+
+        //Set negative button
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Creating AlertDialog
+        AlertDialog dialog = builder.create();
+        //Displaying AlertDialog
+        dialog.show();
     }
 }
